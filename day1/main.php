0315e0abc20be38c57959e5a5546c10ab9c21890
Małek Kamil <k.malek@tvn.pl>
2018-12-05 16:17:27 +0100

    day1 - part two, one repeat of file

--- day1/main.php
+++ day1/main.php
@@ -4,13 +4,27 @@ $content = fopen('input.txt', 'r');
 
 if ($content) {
     $frequency = 0;
+    $frequencyHistory = array(0);
+    $firstRepeatOfFrequency = null;
 
     while (false !== $line = fgets($content)) {
         $operation = substr($line, 0, 1);
         $value = intval(substr($line, 1));
 
         $frequency += $operation == '+' ? $value : (-$value);
+
+        if (is_null($firstRepeatOfFrequency)) {
+            if (in_array($frequency, $frequencyHistory)) {
+                $firstRepeatOfFrequency = $frequency;
+            } else {
+                $frequencyHistory[] = $frequency;
+            }
+        }
     }
 
-    echo $frequency;
+    echo sprintf('Frequency: %d', $frequency);
+
+    if (!is_null($firstRepeatOfFrequency)) {
+        echo sprintf('First repeat of frequency: %d', $firstRepeatOfFrequency);
+    }
 }
